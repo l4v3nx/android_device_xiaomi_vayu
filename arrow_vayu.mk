303462d9f7bb246c94a25418ffa273f47fc5e143
l4v3nx <ygiki52@gmail.com>
2023-04-16 17:11:28 +0300

    [SQUASH] Adapt back for ArrowOS
    Revert "vayu: Bring-up Pixel Experience"
    This reverts commit 33c2795f9684d4b3480a50264be3823a4e3c0eb2
    
    Revert "vayu: Set CUSTOM_DEVICE for Build Fingerprint"
    
    This reverts commit a40c5fcb3b158992fbc4db0a86cfbcfc1c5237b5.
    
    Revert "vayu: Drop build {description, fingerprint} from MIUI"
    
    This reverts commit ca0174511a2f9949280409d1184db827ae022b90.
    
    Revert "vayu: Enable quick tap"
    
    also leave TARGET_USES_AOSP_RECOVERY
    This reverts commit 64ef2789477255a17e1babb6afae79ba7c1cd17f.

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -11,22 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Pixel Experience configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common ArrowOS configurations
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-# Feature Flags
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := arrow_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-CUSTOM_DEVICE := vayu
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.5.0.SJUMIXM release-keys" \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
+
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
+
+# ArrowOS additions
+DEVICE_MAINTAINER := l4v3nx
+ARROW_GAPPS := true
