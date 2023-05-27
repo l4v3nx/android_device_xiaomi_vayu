007441b43b1630ed3502789f2a1f4275a8118f43
l4v3nx <ygiki52@gmail.com>
2023-05-27 23:37:49 +0300

    Revert "vayu: Make shim for com.qti.node.watermark.so"
    looks like we already have this code in libpiex_shim
    ref: https://github.com/ArrowOS/android_hardware_arrow_compat/commit/861345fd7f7ceda50175cb5caebfbd519e766a12
    This reverts commit 655d09e863a4d6a7b292b32b28456eb4bd1aabb3.
    
    Signed-off-by: l4v3nx <ygiki52@gmail.com>

--- device.mk
+++ device.mk
@@ -123,8 +123,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libshim_megvii \
-    libpiex_shim \
-    libwatermark_shim
+    libpiex_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
