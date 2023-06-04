b7c54fa069ee67591c01622580c888b5a29855d1
l4v3nx <ygiki52@gmail.com>
2023-06-04 03:20:36 +0300

    Revert "vayu: sepolicy: Use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS"
    this is causing so much problems, since one partition can't extend sepolicies from another one
    This reverts commit a8c83698ff750f27339e11a5ad2c4337b2b64b13.
    
    Signed-off-by: l4v3nx <ygiki52@gmail.com>
    
    Revert "vayu: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy."
    
    return to arrowos defaults
    This reverts commit 6073de0975793cd424da23a5a0e1f6d29a47923c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,9 +170,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
 VENDOR_SECURITY_PATCH := 2023-05-01
